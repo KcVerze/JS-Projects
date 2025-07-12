07164031617dfd67138d39f8bc16de495d97f4e3
KcVerze <kcverze@gmail.com>
2025-07-12 16:05:21 +0100

    todo list completed

--- Todo List App/index.js
+++ Todo List App/index.js
@@ -1,27 +1,36 @@
 function add(){
     const input = document.getElementById('task-input');
     // to remove white space 
-    const taskText = input.value.trim();
+    const taskValue = input.value.trim();
     // if empty
-    if (taskText === '') {
+    if (taskValue=== '') {
         alert('Enter a new task');
         return;
     }
-    else{
         // create new task
         const li = document.createElement('li');
-        li.textContent = taskText;
+        // the actual task container
+        const taskText = document.createElement('span');
+        taskText.textContent = taskValue;
         // delete btn
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "Delete";
-        deleteBtn.onClick = function(){ li.remove()};
+        deleteBtn.onclick = function(){ li.remove();};
 
-        // to combine the li and the deleteBtn
+        // assemble the li with the task text and delete btn
+        li.appendChild(taskText);
         li.appendChild(deleteBtn);
         // to put the new task below the previous task
-        document.getElementById('tasklist').appendChild(li);
+        document.getElementById('taskList').appendChild(li);
         // to clear input bar
         input.value = '';
-    }
 
+        //checked or unchecked
+        taskText.onclick = function(){taskText.classList.toggle("completed");};
+
+        // edit text
+        taskText.ondblclick = function(){
+            input.value = taskText.textContent;
+            li.remove();
+        };
 }
